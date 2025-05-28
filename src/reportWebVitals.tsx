cf696d7ee0a06803639e74c9d5c6e6ba92dc2cde
trungbt00 <trungbt00@gmail.com>
2025-05-28 11:10:02 +0700

    Update reportWebVitals.tsx

--- src/reportWebVitals.tsx
+++ src/reportWebVitals.tsx
@@ -1,5 +1,5 @@
 import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
-import type { ReportCallback } from 'web-vitals'; // Sử dụng ReportCallback thay vì ReportHandler
+import type { ReportCallback } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportCallback) => {
   if (onPerfEntry && typeof onPerfEntry === 'function') {

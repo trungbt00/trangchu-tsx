1f181c396fa9e00a49bc0c8f14e8c8b157f82cfa
trungbt00 <trungbt00@gmail.com>
2025-05-28 11:06:38 +0700

    Update reportWebVitals.tsx

--- src/reportWebVitals.tsx
+++ src/reportWebVitals.tsx
@@ -1,14 +1,13 @@
-import { ReportHandler } from "web-vitals";
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
+import type { ReportCallback } from 'web-vitals'; // Sử dụng ReportCallback thay vì ReportHandler
 
-const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
-  if (onPerfEntry && typeof onPerfEntry === "function") {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = (onPerfEntry?: ReportCallback) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
